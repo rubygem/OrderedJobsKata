d7312236107f7f521dc99f466b589a09c5c19afe
rubygem <gemma.cameron@gmail.com>
2011-09-30 10:24:21 +0100

    completed step one

---
+++ OrderedJobs.Tests/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+namespace OrderedJobs.Tests
+{
+    [TestFixture]
+    public class TakeEmptyStrings
+    {
+        [Test]
+        public void EmptyStringReturnsEmptySequence()
+        {
+            String input = "";
+            String result = "";
+            Assert.That(result, Is.EqualTo(""));
+        }
+    }
+}
