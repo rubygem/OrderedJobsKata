5b7d2d546e282d49362fe81c7f28541f5e6be2f2
rubygem <gemma.cameron@gmail.com>
2011-10-08 18:41:29 +0100

    refactored to include a private property called Instruction which stores the instruction as a split string

--- OrderedJobs/Job.cs
+++ OrderedJobs/Job.cs
@@ -12,10 +12,12 @@ namespace OrderedJobs
     {
         public Job(string instruction)
         {
-            var split = instruction.Split(new string[]{"=>"}, StringSplitOptions.RemoveEmptyEntries);
-            Name = split[0].Trim();
-            if (split.Length > 1) Dependency = new Job(split[1].Trim());
+            Instruction = instruction.Split(new string[]{"=>"}, StringSplitOptions.RemoveEmptyEntries);
+            Name = Instruction[0].Trim();
+            if (Instruction.Length > 1) Dependency = new Job(Instruction[1].Trim());
         }
+
+        private string[] Instruction { get; set; }
 
         public String Name { get; set; }
 
