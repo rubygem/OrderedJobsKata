928665f1596750afd4d32352faf8852d811efc61
rubygem <gemma.cameron@gmail.com>
2011-10-12 17:58:08 +0100

    Test was wrong, code was right

--- OrderedJobs.Tests/OrderedJobsKata.cs
+++ OrderedJobs.Tests/OrderedJobsKata.cs
@@ -60,7 +60,7 @@ namespace Instructions.Tests
 
             String result = new Instructions(input).ComputeSequence();
 
-            Assert.That(result, Is.EqualTo("afcdbef"));
+            Assert.That(result, Is.EqualTo("afcbde"));
         }
     }
 }
